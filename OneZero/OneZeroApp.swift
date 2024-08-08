e64368c2b05ffcd40ce8e9f646c2eab2beaa3e3c
paramecium <f4ce1ess@outlook.com>
2024-08-08 21:07:35 +0800

    Initial Commit

---
+++ OneZero/OneZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneZeroApp.swift
+//  OneZero
+//
+//  Created by Fish on 8/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct OneZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
