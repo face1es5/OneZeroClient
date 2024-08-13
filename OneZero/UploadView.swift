6c2a9c854e5aebd5b689b6cefeca3d41cc27af57
paramecium <f4ce1ess@outlook.com>
2024-08-13 16:34:11 +0800

    Uploading UI completed partially. Next: split view and uploading to server.

--- OneZero/UploadView.swift
+++ OneZero/UploadView.swift
@@ -9,77 +9,120 @@ import SwiftUI
 import UniformTypeIdentifiers
 import AVFoundation
 
-struct VideoThumbView: View {
-    @State var thumbnail: Image? = nil
-    @State var fetching = true      // fetching thumbnail
-    @State var uploading = false    // uploading to server
+class VideoItem: Identifiable, Hashable ,ObservableObject {
+    let id = UUID().uuidString
     let url: URL
+    let name: String
+    @Published var loadingThumb: Bool = true
+    @Published var uploading: Bool = false
     
-    init(from path: String) {
-        url = URL(fileURLWithPath: path)
+    init(from url: URL) {
+        self.url = url
+        self.name = url.lastPathComponent
+    }
+    init(from url: String) {
+        self.url = URL(fileURLWithPath: url)
+        self.name = self.url.lastPathComponent
     }
     
-    init(from path: URL) {
-        url = path
+    static func == (lhs: VideoItem, rhs: VideoItem) -> Bool {
+        return lhs.id == rhs.id || lhs.url.absoluteString == rhs.url.absoluteString
     }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(url.absoluteString)
+    }
+}
 
+class VideoViewModel: ObservableObject {
+    @Published var videos: [VideoItem] = []
+    
+    func load(from urls: [URL]) {
+        videos =  urls.map { VideoItem(from: $0) }
+    }
+    
+    func load(from urls: [String]) {
+        videos = urls.map { VideoItem(from: $0) }
+    }
+    
+    func count() -> Int { return videos.count }
+}
+
+struct VideoThumbView: View {
+    @ObservedObject var video: VideoItem
+    @State var thumbnail: Image? = nil
+    
     var body: some View {
-        VStack {
-            if fetching {
+        ZStack(alignment: .center) {
+            if video.uploading {
                 ProgressView()
-                    .frame(width: 200, height: 200)
-            } else if thumbnail != nil {
-                VStack {
-                    thumbnail!
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding()
-                }
-                .border(Color(NSColor.lightGray), width: 2)
-                .frame(width: 200, height: 200)
-
-            } else {
-                Button(action: genThumbnail) {
-                    Image(systemName: "arrow.clockwise")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                }
-                .buttonStyle(PlainButtonStyle())
-                .frame(width: 200, height: 200)
+                    .frame(width: 100, height: 100)
+                    .zIndex(1)
             }
-            Text(url.lastPathComponent).frame(maxWidth: 200)
-        }
-        .padding()
-        .task {
-            genThumbnail()
+            VStack {
+                if video.loadingThumb {
+                    ProgressView()
+                        .frame(width: 200, height: 200)
+                } else if thumbnail != nil {
+                    VStack {
+                        thumbnail!
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                            .blur(radius: video.uploading ? 10 : 0)
+                    }
+                    .border(Color(NSColor.lightGray), width: 2)
+                    .frame(width: 200, height: 200)
+
+                } else {
+                    Button(action: genThumbnail) {
+                        Image(systemName: "arrow.clockwise")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 200, height: 200)
+                }
+                Text(video.name).frame(maxWidth: 200)
+            }
+            .padding()
+            .task {
+                genThumbnail()
+            }
         }
     }
     
     func genThumbnail() {
-        defer {
-            fetching = false
-        }
-        print("generating image thumbnail from \(url)")
-        let asset = AVURLAsset(url: self.url, options: nil)
+        video.loadingThumb = true
+        print("generating image thumbnail from \(video.url)")
+        let asset = AVURLAsset(url: video.url, options: nil)
         let img_gen = AVAssetImageGenerator(asset: asset)
         img_gen.appliesPreferredTrackTransform = true
         DispatchQueue.global().async {
+            defer {
+                DispatchQueue.main.async {
+                    video.loadingThumb = false
+                }
+            }
             do {
                 let cgImg = try img_gen.copyCGImage(at: CMTime(seconds: 1.0, preferredTimescale: 60), actualTime: nil)
-                thumbnail = Image(cgImg, scale: 1.0, orientation: .up, label: Text(url.lastPathComponent))
+                DispatchQueue.main.async {
+                    thumbnail = Image(cgImg, scale: 1.0, orientation: .up, label: Text(video.name))
+                }
             } catch let error {
-                print("Some error occurs while generating thumbnail of url arrow.clockwise\(url): \(error)")
+                print("Some error occurs while generating thumbnail of url \(video.url): \(error)")
             }
         }
     }
 }
 
 struct UploadView: View {
-    @State var files: [URL] = []
-    @State private var selectedItems: Set<URL> = []
+    @ObservedObject var videoItems: VideoViewModel = VideoViewModel()
+    @State private var selectedItems: Set<VideoItem> = []
+    
     var body: some View {
-        VStack(spacing: 10) {
+        VStack(alignment: .leading, spacing: 10) {
             HStack {
                 Text("choose files:")
                 Button(action: {    // select files.
@@ -89,7 +132,7 @@ struct UploadView: View {
                     panel.canChooseDirectories = false
                     panel.allowedContentTypes = [UTType.video, UTType.movie, UTType.avi]
                     if panel.runModal() == .OK {
-                        files = panel.urls
+                        videoItems.load(from: panel.urls)
                     }
                 }) {
                     Image(systemName: "folder")
@@ -98,17 +141,13 @@ struct UploadView: View {
             Text("Selected files: ")
             ScrollView(.horizontal) {
                 LazyHStack(spacing: 10) {
-                    ForEach($files, id:\.self) { $url in
-                        VideoThumbView(from: url)
-                            .background( selectedItems.contains(url) ? Color.accentColor : .clear)
+                    ForEach(videoItems.videos) { video in
+                        VideoThumbView(video: video)
+                            .background( selectedItems.contains(video) ? Color.accentColor : .clear)
                             .cornerRadius(15)
                             .onTapGesture {
-                                if selectedItems.contains(url) {
-                                    selectedItems.remove(url)
-                                } else {
-                                    selectedItems.insert(url)
-                                }
-                            }
+                                if selectedItems.contains(video) { selectedItems.remove(video) }
+                                else { selectedItems.insert(video) }}
                     }
                 }
             }
@@ -118,34 +157,44 @@ struct UploadView: View {
         .padding()
         .toolbar {
             Button(action: {
-                DispatchQueue.global().async {
-                    upload()
+                Task {
+                    await upload()
                 }
             }) {
-                Label("upload videos.", systemImage: "square.and.arrow.up")
+                Image(systemName: "square.and.arrow.up")
             }
             .disabled((selectedItems.count > 0) ? false: true)
         }
     }
     
-    func upload() {
-        print("Uploading in thread: \(Thread.current)")
-        print("Selected items:")
-        if selectedItems.count > 0 {
-            for url in selectedItems {
-                print(url.lastPathComponent)
-            }
-        } else {
-            print("No video selected. Cancel uploading.")
+    func upload(for video: VideoItem) async throws {
+        await MainActor.run {
+            video.uploading = true
         }
-
+        defer { Task { @MainActor in video.uploading = false } }
+        
+        //simulate loading data and uploading to server...
+//        let data = try Data(contentsOf: video.url)
+        try await Task.sleep(nanoseconds: 2_000_000_000)
+    }
+    
+    func upload() async {
+        guard selectedItems.count > 0 else { print("No videos selected.");return }
+        for video in selectedItems {
+            Task {
+                do {
+                    try await upload(for: video)
+                } catch let error {
+                    print("Error when uploading \(video.name): \(error)")
+                }
+            }
+        }
     }
 }
 
 struct UploadView_Previews: PreviewProvider {
     static var previews: some View {
         UploadView()
-        VideoThumbView(from: "file:///Users/fish/Desktop/sample.mp4")
     }
 }
 
